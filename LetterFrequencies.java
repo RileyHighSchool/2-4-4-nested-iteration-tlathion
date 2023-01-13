c6e9be0e8c69944991fea9902e70a27c2078b1b8
tlathion <trystan90205@students.sbcsc.k12.in.us>
2023-01-13 13:42:35 -0500

    finished

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -11,5 +11,17 @@ public class LetterFrequencies
 		System.out.println("The following shows the letter frequencies for the phrase");
 
     /* your code here */
+		for (int i = 0; i <26; i++){
+			String let = letters.substring(i, i + 1);
+			int count = 0;
+			for(int k = 0; k < phrase.length(); k++){
+				String lett = phrase.substring(k,k+1);
+				if(let.equals(lett)){
+					count = count + 1;
+
+				}
+			}
+			System.out.println("There is/are " + count + " " + let + "'s ' in the phrase. ");
+	   }
 	}
 }
